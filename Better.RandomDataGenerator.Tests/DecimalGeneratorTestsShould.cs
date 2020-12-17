f75bc5303f71ebf08575fdec82341875acbdde6a
Niklas Lööw <niklas.loow@cdon.com>
2020-12-17 22:52:31 +0100

    1.0.1 on nuget

---
+++ Better.RandomDataGenerator.Tests/DecimalGeneratorTestsShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Better.RandomDataGenerator.Tests
+{
+    public class DecimalGeneratorTestsShould
+    {
+        [Fact]
+        public void ReturnRandomValues()
+        {
+            
+        }
+    }
+}
---
+++ Better.RandomDataGenerator/DecimalGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Better.RandomDataGenerator
+{
+    public static partial class RandomDataGenerator
+    {
+        public static class DecimalGenerator
+        {
+            public static decimal GetRandomDecimal()
+            {
+                var randomInt = IntGenerator.GetRandomNumberInRange(100, 10000000);
+                return randomInt / 10.0m;
+            }
+        }
+    }
+}
